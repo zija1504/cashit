e207f0fe1ef0d2f28bb7387dc96643137ea355fb
Zija <zija1504@gmail.com>
2020-01-05 00:18:06 +0100

    create project structure

---
+++ tests/test_cashit.py
@@ -0,0 +1,14 @@
+from cashit import __version__
+
+
+def test_version():
+    assert __version__ == "0.1.0"
+
+
+test = [
+    "allsadsadsadasdsadasdasdadasdasd",
+    "asdsadasdassadadsdadasd",
+    "asdasdasd",
+    "sa",
+]
+

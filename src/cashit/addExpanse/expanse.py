01d30d5b4302af255ad06a0be66655efa0f17f3e
Zija <zija1504@gmail.com>
2020-01-05 03:18:57 +0100

    first function

---
+++ src/cashit/addExpanse/expanse.py
@@ -0,0 +1,8 @@
+import click
+
+
+@click.command()
+def add_expanse():
+    """Add your expanse to database"""
+    while True:
+        pass
--- src/cashit/cli.py
+++ src/cashit/cli.py
@@ -1,10 +1,13 @@
 import click
+from .addExpanse.expanse import add_expanse
 
 
 @click.group()
 def cli():
     """Command line interface for the cashit package"""
 
+
+cli.add_command(add_expanse)
 
 if __name__ == "__main__":
     cli()
